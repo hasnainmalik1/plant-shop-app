d3545788f0f46fa43b9e421a6060f72424f66cee
hasnalinmalik <hasnainmalik6748@gmail.com>
2025-11-03 17:05:04 +0500

    secon commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/plant-shop-app/',  // <-- set this to your repo name preceded and followed by '/'
+  // other config...
 })
